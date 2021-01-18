85caf2a7a6f90c875546edd41b5b8dad95dc0948
whykayallday <youngjoonkimm@gmail.com>
2021-01-18 01:10:00 -0800

    completed repeatString.js, tested with jasmine

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function() {
-
+    let testStr = '';
+    for(let i = 0; i < 3; i++){
+        testStr += 'hey';
+    }
+    return testStr;
 }
 
 module.exports = repeatString
