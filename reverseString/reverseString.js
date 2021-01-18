572600b86fbcceecb9ca77c78f0fe71dab2700b9
whykayallday <youngjoonkimm@gmail.com>
2021-01-18 15:48:28 -0800

    completed reverseString, tested with jasmine

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseStr = '';
+    for(let i = str.length; i >= 0; --i){
+        reverseStr += str.charAt(i);
+    }
+    return reverseStr;
 }
+
 
 module.exports = reverseString
