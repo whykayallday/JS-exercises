ce840c3abc0ca28ff001ab1e845fd4aedb7ef057
whykayallday <youngjoonkimm@gmail.com>
2021-01-19 00:30:27 -0800

    retested removeFromArray with all specs passed via jasmine

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,8 +1,17 @@
 const removeFromArray = function(arrayInput, ...element) {
     let modifiedArray = [];
+    
 
     for(let i = 0; i < arrayInput.length; i++) {
-        if(arrayInput[i] == element) {
+        let matchFound = false;
+        
+        for(let j = 0; j < element.length; j++) {
+            if(arrayInput[i] === element[j]) {
+                matchFound = true;
+                break;
+            }
+        }
+        if(matchFound == true) {
             continue;
         }
         else {
