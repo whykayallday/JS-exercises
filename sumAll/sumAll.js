4115239c6a41bb3413cc0b36a73db3db67bd710c
whykayallday <youngjoonkimm@gmail.com>
2021-01-18 21:13:32 -0800

    completed sumAll, tested with Jasmine

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let sumValue = 0;
 
+    for(let i = firstNum; i <= lastNum; i++){
+        sumValue += i;
+    }
+    return sumValue;
 }
 
 module.exports = sumAll
