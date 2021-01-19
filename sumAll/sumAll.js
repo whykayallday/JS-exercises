0ef525911aec9849411850972a797653d05c844e
whykayallday <youngjoonkimm@gmail.com>
2021-01-19 00:53:04 -0800

    retested sumAll - 2 failures remaining

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,6 +1,17 @@
 const sumAll = function(firstNum, lastNum) {
     let sumValue = 0;
 
+    if (!Number.isInteger(firstNum || !Number.isInteger(lastNum))){
+        return 'ERROR';
+    }
+    else if(firstNum <= 0 || lastNum <= 0) {
+        return 'ERROR';
+    }
+    else if(firstNum > lastNum) {
+        let buffer = firstNum;
+        firstNum = lastNum;
+        lastNum = buffer; 
+    }
     for(let i = firstNum; i <= lastNum; i++){
         sumValue += i;
     }
