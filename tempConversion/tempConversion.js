95f5606744b5c48d211eebe49fdae9895b0412c2
whykayallday <youngjoonkimm@gmail.com>
2021-01-19 22:56:53 -0800

    completed tempConversion and all specs tested with jasmine

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,27 @@
-const ftoc = function() {
+const ftoc = function(inputTempF) {
+  let tempConversion = (inputTempF - 32) / 1.8;
 
+  tempConversion = roundToFixed(tempConversion);
+
+  return Number(tempConversion);
 }
 
-const ctof = function() {
 
+const ctof = function(inputTempC) {
+  let tempConversion = (inputTempC * 1.8) + 32;
+
+  tempConversion = roundToFixed(tempConversion);
+
+  return Number(tempConversion);
 }
+
+
+function roundToFixed(inputNum) {
+  let fixedNum = (inputNum * 10) / 10;
+
+  return fixedNum.toFixed(1);
+}
+
 
 module.exports = {
   ftoc,
